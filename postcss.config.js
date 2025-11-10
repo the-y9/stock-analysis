78130e1469b02221d1f3a97bb86af250a3a447f1
Yash Mishra <ymishra975@gmail.com>
2025-11-10 23:00:23 +0530

    css

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,12 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  }
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'color-functional-notation': true,
+        'relative-color-syntax': true,
+      },
+    },
+  },
 };
